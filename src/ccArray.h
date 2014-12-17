31d782340edfce8a4518b0077aaef966a5dcf234
Piotr Hałas <piotr@halas.net.pl>
2014-12-17 11:06:01 +0100

    simple refactoring

--- src/ccArray.h
+++ src/ccArray.h
@@ -8,13 +8,134 @@
 #ifndef CCARRAY_H_
 #define CCARRAY_H_
 
-namespace ccArray {
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <stdio.h>
+
+namespace cc_array {
 
 class ccArray {
+private:
+	/**
+	 * Value of node
+	 */
+	std::string value;
+
+	/**
+	 * Node name
+	 */
+	std::string name;
+
+	/**
+	 * Vector of node childs
+	 */
+	std::vector<ccArray *> childs;
+
+	/**
+	 * is node set (not null)
+	 */
+	bool isset;
+
+	/**
+	 * is node array (not value, not list)
+	 */
+	bool is_array;
+
+	/**
+	 * has node childs
+	 */
+	bool has_valid_childs;
+
+	/**
+	 * last inserted key, for list node
+	 */
+	int last_key_id;
+
+	/**
+	 * pointer to parent node
+	 */
+	ccArray *parent;
+
+
+	void _setParent(ccArray *parent);
+
+	static int _string2int(std::string value);
+	static std::string _int2string(int value, std::string format = "%d");
 public:
+	/**
+	 * Node types
+	 */
+	static const int TYPE_NOT_SET = 0;
+	static const int TYPE_VALUE = 1;
+	static const int TYPE_ARRAY = 2;
+	static const int TYPE_LIST = 3;
+
+	std::string getName();
+	std::string getValue();
+
+	//Constructors
 	ccArray();
-	virtual ~ccArray();
+	ccArray(std::string name);
+	//Copy constructor
+	ccArray(ccArray &b);
+
+	/**
+	 * Custom Destructor
+	 */
+	~ccArray();
+
+	/**
+	 * unset one child by key
+	 */
+	void unset(std::string key);
+
+	/**
+	 * Return all children's
+	 */
+	std::vector<ccArray *> getChildrens();
+
+	/**
+	 * Get parent node
+	 * Return null if root
+	 */
+	ccArray *getParent();
+
+	/**
+	 * Get node type
+	 */
+	int getType() const;
+
+	/**
+	 * operators overload
+	 */
+	bool operator!=(ccArray &b);
+	bool operator==(ccArray &b);
+	ccArray &operator=(ccArray &b);
+	ccArray &operator=(std::string value);
+	ccArray& operator[](std::string key);
+	operator std::string() const;
+	operator void*();
+	friend std::ostream& operator<<(std::ostream& os, const ccArray& dt);
+
+
+	/**
+	 * Return as string
+	 */
+	std::string toString() const;
+
+	/**
+	 * check if node has valid childrens
+	 */
+	bool hasChildrens();
+
+	/**
+	 * Append node
+	 */
+	ccArray& append();
+
 };
+
 
 } /* namespace ccArray */
 
--- src/main.cc
+++ src/main.cc
@@ -1,302 +1,12 @@
-#include <iostream>
-#include <sstream>
-#include <vector>
-#include <stdio.h>
-class Node {
-private:
-	std::string value;
-	std::string name;
-	std::vector<Node *> childs;
-	bool isset;
-	bool is_array;
-	bool has_valid_childs;
-	int last_key_id;
-	Node *parent;
-	void setParent(Node *parent) {
-		this->parent = parent;
-	}
+#include "ccArray.h"
 
-public:
-	static const int TYPE_NOT_SET = 0;
-	static const int TYPE_VALUE = 1;
-	static const int TYPE_ARRAY = 2;
-	static const int TYPE_LIST = 3;
+using namespace cc_array;
 
-	static int string2int(std::string value) {
-		int tmp;
-		std::istringstream(value) >> tmp;
-		return tmp;
-	}
-
-	/**
-	 * Return name o this node
-	 */
-	std::string getName() {
-		return this->name;
-	}
-
-	/**
-	 * Return value of this node as string
-	 * If array or list return "ARRAY"
-	 * see toString function
-	 */
-	std::string getValue() {
-		return this->toString();
-	}
-
-	static std::string int2string(int value, std::string format = "%d") {
-		char buffer[50];
-		sprintf(buffer, format.c_str(), value);
-		return buffer;
-	}
-
-	Node() {
-		this->isset = false;
-		this->last_key_id = 0;
-		this->is_array = false;
-		this->parent = NULL;
-		this->has_valid_childs = false;
-	}
-	Node(std::string name) {
-		this->name = name;
-		this->isset = false;
-		this->last_key_id = 0;
-		this->is_array = false;
-		this->parent = NULL;
-		this->has_valid_childs = false;
-	}
-
-	/**
-	 * Destructor
-	 */
-	~Node() {
-		//delete all chidrens
-		for (std::vector<Node *>::iterator it = childs.begin();
-				it != childs.end(); ++it) {
-			delete (*it);
-		}
-	}
-
-	/**
-	 * unset one child
-	 */
-	void unset(std::string key) {
-		//find key and delete
-		for (std::vector<Node *>::iterator it = childs.begin();
-				it != childs.end();) {
-			if ((*it)->getName() == key) {
-				it = childs.erase(it);
-			} else {
-				it++;
-			}
-		}
-	}
-
-	/**
-	 * Retrun all childrens
-	 */
-	std::vector<Node *> getChildrens() {
-		return this->childs;
-	}
-
-	/**
-	 * Get parent node
-	 * Return null if root
-	 */
-	Node *getParent() {
-		return this->parent;
-
-	}
-
-	/**
-	 * Get node type
-	 */
-	int getType() const {
-		if (this->childs.size() > 0 && this->has_valid_childs) {
-			if (this->is_array)
-				return TYPE_ARRAY;
-			return TYPE_LIST;
-		}
-		if (this->isset)
-			return TYPE_VALUE;
-		return TYPE_NOT_SET;
-	}
-
-	/**
-	 * != operator overload
-	 */
-	bool operator!=(Node &b) {
-		return (*this == b) ? false : true;
-	}
-
-	/**
-	 * == operator overload
-	 */
-	bool operator==(Node &b) {
-		//test if type is equal
-		if (this->getType() != b.getType())
-			return false;
-
-		switch (this->getType()) {
-		case TYPE_NOT_SET:
-			return true;
-			break;
-		case TYPE_VALUE:
-			//test only the value
-			return (this->value == b.value) ? true : false;
-			break;
-		case TYPE_ARRAY:
-		case TYPE_LIST:
-			// check if size is the same
-			if (this->childs.size() != b.childs.size()) {
-				return false;
-			}
-
-			//test childs if equal
-			for (std::vector<Node *>::iterator it = childs.begin();
-					it != childs.end(); it++) {
-
-				//check if key exists
-				if (!b[(*it)->getName()]) {
-					return false;
-				}
-
-				//check if nodes are equal, recurency
-				if ((b[(*it)->getName()] != (**it)))
-					return false;
-			}
-
-			return true;
-			break;
-		}
-		return false;
-	}
-
-	/**
-	 * Return as string
-	 *
-	 */
-	std::string toString() const {
-		switch (this->getType()) {
-		case TYPE_NOT_SET:
-			return "NULL";
-			break;
-		case TYPE_VALUE:
-			return this->value;
-			break;
-		case TYPE_ARRAY:
-			return "Array";
-			break;
-		case TYPE_LIST:
-			return "Array";
-			break;
-		}
-		return "";
-	}
-
-	/**
-	 * check if node has valid childrens
-	 */
-	bool hasChildrens() {
-		return this->has_valid_childs;
-	}
-
-	/**
-	 * Value assign
-	 */
-	Node& operator=(std::string value) {
-		//cleanup some data
-		this->isset = true;
-		this->childs.clear();
-		this->is_array = false;
-		this->has_valid_childs = false;
-		this->value = value;
-
-		//update parent nodes
-		Node *tmp = this->parent;
-		while (tmp) {
-			tmp->isset=true;
-			if (tmp->childs.size() > 0) {
-				tmp->has_valid_childs = true;
-			}
-			tmp = tmp->getParent();
-		}
-		return *this;
-	}
-
-	/**
-	 * Append node
-	 */
-	Node& append() {
-		Node *tmp = new Node(Node::int2string(last_key_id));
-		this->childs.push_back(tmp);
-		this->last_key_id++;
-		return *tmp;
-	}
-
-	Node& operator[](std::string key) {
-		for (std::vector<Node *>::iterator it = this->childs.begin();
-				it != this->childs.end(); ++it) {
-			if ((*it)->name == key) {
-				return *(*it);
-			}
-		}
-		Node *tmp = NULL;
-
-		if (key == "" || key == Node::int2string(Node::string2int(key))) {
-			if (key == "")
-				key = Node::int2string(this->last_key_id);
-			tmp = new Node(key);
-			if (Node::string2int(key) >= this->last_key_id)
-				this->last_key_id = Node::string2int(key) + 1;
-		} else {
-			//this is not list, this is map!
-			// this->is_array = true;
-			tmp = new Node(key);
-		}
-
-		tmp->setParent(this);
-		this->childs.push_back(tmp);
-		return *tmp;
-	}
-
-	operator std::string() const {
-		return this->toString();
-	}
-
-	friend std::ostream& operator<<(std::ostream& os, const Node& dt) {
-		os << dt.toString();
-		return os;
-	}
-
-	operator void*() {
-		if (isset == true)
-			return this;
-		//to do cleanup search path
-		Node *tmp = this;
-		while (tmp->parent && tmp->parent->has_valid_childs == false) {
-			tmp = tmp->parent;
-		}
-		//std::cout<<tmp->parent->getName()<<std::endl;
-		tmp->parent->unset(tmp->getName());
-
-		return 0;
-		//return isset==true ? this : 0;
-	}
-
-	void print();
-};
-
-void Node::print() {
-	std::cout << "Node::" << this->name << ": " << this->toString()
-			<<", Type: " <<this->getType()<<std::endl;
-}
-
-void node_get_leafs(Node *root, std::vector<Node *> *leafs) {
+void node_get_leafs(ccArray *root, std::vector<ccArray *> *leafs) {
 
 	if (root->hasChildrens()) {
-		std::vector<Node *> tmp = root->getChildrens();
-		for (std::vector<Node *>::iterator it = tmp.begin(); it != tmp.end();
+		std::vector<ccArray *> tmp = root->getChildrens();
+		for (std::vector<ccArray *>::iterator it = tmp.begin(); it != tmp.end();
 				++it) {
 
 			node_get_leafs((*it), leafs);
@@ -308,17 +18,17 @@ void node_get_leafs(Node *root, std::vector<Node *> *leafs) {
 	}
 }
 
-std::string build_http_query(Node *root) {
-	std::vector<Node *> leafs;
+std::string build_http_query(ccArray *root) {
+	std::vector<ccArray *> leafs;
 
 	node_get_leafs(root, &leafs);
 	std::string query = "";
 
-	for (std::vector<Node *>::iterator it = leafs.begin(); it != leafs.end();
+	for (std::vector<ccArray *>::iterator it = leafs.begin(); it != leafs.end();
 			++it) {
 		std::string name = "";
 		std::string last_name = (*it)->getName();
-		Node *parent = (*it);
+		ccArray *parent = (*it);
 		while (parent->getParent()->getParent()) {
 			name = "[" + parent->getName() + "]" + name;
 			last_name = parent->getName();
@@ -334,7 +44,7 @@ std::string build_http_query(Node *root) {
 
 int main(void) {
 
-	Node test;
+	ccArray test;
 	test["id"] = "1";
 	test["title"] = "test titile";
 	test["book"]["1"]["name"] = "1";
@@ -349,9 +59,10 @@ int main(void) {
 	test["autoinc"]["abc"] = "abc";
 	test["autoinc"][""] = "9";
 
-	Node test1;
+	ccArray test1;
 	test1["1"]["Name"] = "2";
 	test1["2"]["Name"] = "2";
+	ccArray test2 = test1;
 
 
 	if (test1["1"] == test1["2"]) {
@@ -367,11 +78,11 @@ int main(void) {
 		std::cout << "Check ok\n";
 	}
 
-	std::vector<Node *> leafs;
-	node_get_leafs(&test, &leafs);
-	for (std::vector<Node *>::iterator it = leafs.begin(); it != leafs.end();
+	std::vector<ccArray *> leafs;
+	node_get_leafs(&test1, &leafs);
+	for (std::vector<ccArray *>::iterator it = leafs.begin(); it != leafs.end();
 			++it) {
-		Node *parent = (*it);
+		ccArray *parent = (*it);
 		std::string name = "";
 		while (parent->getParent()) {
 			name = parent->getName() + name;
@@ -382,4 +93,21 @@ int main(void) {
 		std::cout << name << "=" << (*it)->getValue() << std::endl;
 	}
 
+	std::cout<<"Using copy!"<<std::endl;
+	leafs.clear();
+	node_get_leafs(&test2, &leafs);
+	for (std::vector<ccArray *>::iterator it = leafs.begin(); it != leafs.end();
+			++it) {
+		ccArray *parent = (*it);
+		std::string name = "";
+		while (parent->getParent()) {
+			name = parent->getName() + name;
+			parent = parent->getParent();
+			if (parent->getParent())
+				name = "." + name;
+		}
+		std::cout << name << "=" << (*it)->getValue() << std::endl;
+	}
+	return 0;
 }
+
